4de108a418667da9d314d84c5d8713cc1db77b8d
vipints <vipin@cbio.mskcc.org>
2014-02-18 14:28:16 -0800

    -fixed the installation of pbs_python

---
+++ pbs_interface.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python  
+"""
+The main pbs module
+"""
+
+import os 
+import re 
+import sys
+
+HAVE_PBS_PYTHON = True
+try:
+    import pbs
+    from PBSQuery import PBSQuery
+except:
+    print 'Failed to import pbs_python module'
+    HAVE_PBS_PYTHON = False
+
+def get_query():
+    """
+    Return query instance
+    """
+    return PBSQuery()
